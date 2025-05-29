9585f49b347a35d408df4b68621af66c070afa1b
George Mihailovski <georgemihailovski@gmail.com>
2025-05-29 20:55:27 +1000

    Initial Commit

---
+++ Panda Log/Panda_LogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Panda_LogApp.swift
+//  Panda Log
+//
+//  Created by George Mihailovski on 29/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct Panda_LogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Panda LogTests/Panda_LogTests.swift
@@ -0,0 +1,17 @@
+//
+//  Panda_LogTests.swift
+//  Panda LogTests
+//
+//  Created by George Mihailovski on 29/5/2025.
+//
+
+import Testing
+@testable import Panda_Log
+
+struct Panda_LogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
